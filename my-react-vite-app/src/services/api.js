b4ace56d8801ce7c66cbd095e7ce74d1fdfac3fa
Aditya-Chaudhary <adityachaudhary7724@gmail.com>
2025-01-13 22:35:43 +0530

    React App

---
+++ my-react-vite-app/src/services/api.js
@@ -0,0 +1,19 @@
+
+const API_KEY = "4c6cbb6383a004fc2bf4f4fc58078dd8";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
